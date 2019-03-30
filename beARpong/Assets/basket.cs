c82a7f79aed7c0788360b8f5b013a5ec6b7c8bf6
ComSciStudent <32064740+ComSciStudent@users.noreply.github.com>
2019-03-31 00:40:18 +0800

    score UI and cups disappearing
    ball collides with cube found inside cup and then cup goes away.
    
    ball still needs to despawn and respawn back to original place.
    
    reset doesn't reset score(?)

---
+++ beARpong/Assets/basket.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class basket : MonoBehaviour
+{
+	public static int scoreValue=0; //reference to the ScoreText gameobject, set in editor
+
+	public Text scoreText;
+	
+	//public Text text;
+    
+    void Awake()
+    {
+        scoreText.text = "Score: 0";
+    }
+
+	void OnCollisionEnter() //if ball hits board
+    {
+         scoreValue++;
+         Debug.Log(scoreValue);
+         scoreText.text = "Score: " + scoreValue.ToString();
+		 Destroy (transform.parent.gameObject);
+         Destroy (gameObject);
+        
+    }
+ 
+
+}
