11ca020a27d1ce7f2824df1833a368988d528584
Aric Brillantes <aric_brillantes@dlsu.edu.ph>
2019-04-02 15:13:37 +0800

    changed score to attempts

--- beARpong/Assets/throwScriptv2.cs
+++ beARpong/Assets/throwScriptv2.cs
@@ -7,8 +7,8 @@ public class throwScriptv2 : MonoBehaviour {
 	Vector2 startPos, endPos, direction; // touch start position, touch end position, swipe direction
 	float touchTimeStart, touchTimeFinish, timeInterval; // to calculate swipe time to sontrol throw force in Z direction
 
-	// [SerializeField]
-	// float throwForceInXandY = 0.5f; // to control throw force in X and Y directions
+	public static int scoreValue=0; //reference to the ScoreText gameobject, set in editor
+	public Text scoreText;
 
 	[SerializeField]
 	float throwForceInZ = 35f; // to control throw force in Z direction
@@ -23,6 +23,7 @@ public class throwScriptv2 : MonoBehaviour {
 
 	void Start()
 	{
+		scoreText.text = "Attempts: 0";
 		rb = GetComponent<Rigidbody> ();
 	}
 
@@ -34,18 +35,16 @@ public class throwScriptv2 : MonoBehaviour {
 
 	public void Release()
 	{
-		 Debug.Log("Release");
+		// marking time when you release it
+		touchTimeFinish = Time.time;
 
-			// marking time when you release it
-			touchTimeFinish = Time.time;
+		// calculate swipe time interval 
+		timeInterval = touchTimeFinish - touchTimeStart;
 
-			// calculate swipe time interval 
-			timeInterval = touchTimeFinish - touchTimeStart;
-
-			// add force to balls rigidbody in 3D space depending on swipe time, direction and throw forces
-			rb.isKinematic = false;
-            rb.AddForce(0,100f,throwForceInZ / timeInterval);
-			StartCoroutine(ResetAfterTime(2f));
+		// add force to balls rigidbody in 3D space depending on swipe time, direction and throw forces
+		rb.isKinematic = false;
+		rb.AddForce(0,100f,throwForceInZ / timeInterval);
+		StartCoroutine(ResetAfterTime(2f));
 			
         // if(Input.GetMouseButtonDown (0)){
         //     Debug.Log("Mouse Down");
@@ -62,6 +61,10 @@ public class throwScriptv2 : MonoBehaviour {
 
 	public void Reset()
 	{
+		scoreValue++;
+        //  Debug.Log(scoreValue);
+        scoreText.text = "Attempts: " + scoreValue.ToString();
+
 		rb.isKinematic = true;
 		// ball.transform.position = new Vector3(0.0f,-0.12f,0.383f);
 		ball.transform.localPosition = new Vector3(0.0f,-0.12f,0.383f);
@@ -71,6 +74,11 @@ public class throwScriptv2 : MonoBehaviour {
 	public IEnumerator ResetAfterTime(float passtime){
 		yield return new WaitForSeconds(passtime);
 		Debug.Log("Reset");
+
+		scoreValue++;
+        //  Debug.Log(scoreValue);
+        scoreText.text = "Attempts: " + scoreValue.ToString();
+
 		rb.isKinematic = true;
 		// ball.transform.position = new Vector3(0.0f,-0.12f,0.383f);
 		ball.transform.localPosition = new Vector3(0.0f,-0.12f,0.383f);
