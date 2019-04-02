f9518f0e68df4acc1a2f31894daa104134fa6b9d
Aric Brillantes <aric_brillantes@dlsu.edu.ph>
2019-04-02 22:02:55 +0800

    Revert "updated attempts"
    This reverts commit 893a6d5ef964b5e0052c0c2903cce66d97d32daf.

--- beARpong/Assets/basket.cs
+++ beARpong/Assets/basket.cs
@@ -7,14 +7,19 @@ public class basket : MonoBehaviour
 	private static int cups=0; 
     public GameObject canvas1;
     public GameObject winCanvas;
+	// public Text scoreText;
+	
+	//public Text text;
     
     void Awake()
     {
+        // scoreText.text = "Score: 0";
         
     }
 
     void Start()
     {
+        // scoreText.text = "Score: 0";
         cups=0;
     }
 
@@ -22,7 +27,7 @@ public class basket : MonoBehaviour
     {
         cups++;
         Debug.Log(cups);
-        
+        //  scoreText.text = "Score: " + scoreValue.ToString();
         Destroy (transform.parent.gameObject);
         Destroy (gameObject);
 
